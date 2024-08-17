c2d1fb6b4978101114856e6648c8011f08025335
0-keun <dudrms7352@naver.com>
2024-08-18 00:19:08 +0900

    update_small_mistake

--- scripts/1_imshow.py
+++ scripts/1_imshow.py
@@ -2,9 +2,6 @@ import cv2
 
 image = cv2.imread('./image/JackDaniels.jpg')
 
-print(type(image))
-print(image)
-
-cv2.imshow("Bottle Image", image)
+cv2.imshow("Image", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
